a5c032d04069dc9273a4a6d308a7866bb090f860
Hriday Agrawal <hriday.agrawal@pathlock.com>
2024-09-26 10:53:34 +0530

    changed activate.sh to run all lines of commands

--- Excel_to_DB_Converter/setup.bat
+++ Excel_to_DB_Converter/setup.bat
@@ -1,5 +1,6 @@
 call python -m venv env
 call env/Scripts/activate
 call pip install -r requirements.txt
-call echo env/Scripts/activate > activate.bat
-call echo python xlsx_to_db_converter.py >> activate.bat
+call echo call .\env\Scripts\activate > activate.bat
+call echo call python xlsx_to_db_converter.py >> activate.bat
+
